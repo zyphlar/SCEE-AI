c7f65e320c9b60aee0573ddd8052dba42b7cd842
zyphlar <zyphlar@gmail.com>
2026-03-27 22:20:21 -0700

    add consistent debug key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -288,6 +288,12 @@ android {
     }
 
     signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
         create("release") {
         }
     }
