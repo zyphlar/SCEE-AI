079bfe0f3aeb39d2fd90e2fc29e5be7ddcd3e3f3
zyphlar <zyphlar@gmail.com>
2026-03-29 21:59:40 -0700

    Fix OsmChange: omit changeset attribute to satisfy JOSM validation

--- app/src/main/java/de/westnordost/streetcomplete/voicemapper/VoiceMapperExporter.kt
+++ app/src/main/java/de/westnordost/streetcomplete/voicemapper/VoiceMapperExporter.kt
@@ -40,7 +40,7 @@ object VoiceMapperExporter {
             for (edit in creates) {
                 val pos = edit.position ?: continue
                 val id = nextId--
-                sb.append("    <node id=\"$id\" lat=\"${pos.latitude}\" lon=\"${pos.longitude}\" version=\"0\" changeset=\"-1\">\n")
+                sb.append("    <node id=\"$id\" lat=\"${pos.latitude}\" lon=\"${pos.longitude}\" version=\"0\">\n")
                 for ((k, v) in edit.tags) {
                     sb.append("      <tag k=\"${k.xmlEscape()}\" v=\"${v.xmlEscape()}\"/>\n")
                 }
@@ -66,7 +66,7 @@ object VoiceMapperExporter {
                 val pos = edit.position
                 val tag = key.type.name.lowercase()
                 val posAttrs = if (pos != null) " lat=\"${pos.latitude}\" lon=\"${pos.longitude}\"" else ""
-                sb.append("    <$tag id=\"${key.id}\"$posAttrs version=\"1\" changeset=\"-1\">\n")
+                sb.append("    <$tag id=\"${key.id}\"$posAttrs version=\"1\">\n")
                 for ((k, v) in edit.tags) {
                     sb.append("      <tag k=\"${k.xmlEscape()}\" v=\"${v.xmlEscape()}\"/>\n")
                 }
@@ -86,7 +86,7 @@ object VoiceMapperExporter {
                 val pos = edit.position
                 val tag = key.type.name.lowercase()
                 val posAttrs = if (pos != null) " lat=\"${pos.latitude}\" lon=\"${pos.longitude}\"" else ""
-                sb.append("    <$tag id=\"${key.id}\"$posAttrs version=\"1\" changeset=\"-1\"/>\n")
+                sb.append("    <$tag id=\"${key.id}\"$posAttrs version=\"1\"/>\n")
             }
             sb.append("  </delete>\n")
         }
